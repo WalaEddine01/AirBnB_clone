2d89c5fec0f78bdd29aead5e8575183a27ba5045
WalaEddine01 <walaaeddine33@gmail.Com>
2023-11-09 10:10:35 +0000

    i solve the second task

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,19 @@ from uuid import uuid4
 
 class BaseModel:
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            del kwargs["__class__"]
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    obj_time = datetime.strptime(val, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, obj_time)
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{BaseModel.__name__}] ({self.id}) {self.__dict__}"
