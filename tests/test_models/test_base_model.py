d62aae6f8021cf28f461d630536df397d3b0c471
WalaEddine01 <walaaeddine33@gmail.Com>
2023-11-13 21:26:38 +0000

    I made the changes on 2023-11-13 21:26:38

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,14 +6,26 @@ this module is for Unittesting BaseModel Class
 import unittest
 from models.base_model import BaseModel
 from datetime import datetime
-import os
+from models.engine.file_storage import FileStorage
+import time
 
 
 class TestBaseModel(unittest.TestCase):
+    """
+    This class to test BaseModel Class
+    """
     def test_uuid(self):
         """Test that the id is a string"""
         model = BaseModel()
         self.assertIsInstance(model.id, str)
+
+    def test_save(self):
+        """
+        Testing save method
+        """
+        model = BaseModel()
+        model.save()
+        self.assertNotEqual(model.updated_at, model.created_at)
 
 
 if __name__ == "__main__":
